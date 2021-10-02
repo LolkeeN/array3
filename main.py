89fae98ccc161f65534237947ad552a14b7e4765
Vasyl Veselov <vasyl_veselov@epam.com>
2021-10-02 22:16:13 +0300

    created main function with all functions call

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from TK_1.TK_1 import createListFromInputNumbers
+from TK_2.TK_2 import cortegeWithMinAndMax
+from TK_3.TK_3 import someFunction
+from TK_4.TK-4 import multiplyFunction
+from TK_4.TK-5 import sqrtFunction
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print("**********TASK1***************")
+    num = input("Enter number of numbers for TK_1")
+    createListFromInputNumbers(num)
+    print("**********TASK2***************")
+    cortegeWithMinAndMax(list)
+    print("**********TASK3***************")
+    someFunction(list)
+    print("**********TASK4***************")
+    multiplyFunction()
+    print("**********TASK5***************")
+    sqrtFunction(list)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
